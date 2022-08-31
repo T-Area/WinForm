46e7c57cf3400876100d8df126972f3307177df3
T-Area <111098226+T-Area@users.noreply.github.com>
2022-08-31 12:23:32 +0800

    Add files via upload

---
+++ 记事本应用程序/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace 记事本应用程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmOpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.格式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.样式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开记录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmViserble = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbShow = new System.Windows.Forms.TextBox();
+            this.panelFather = new System.Windows.Forms.Panel();
+            this.btnviserble = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.panelFather.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.格式ToolStripMenuItem,
+            this.样式ToolStripMenuItem,
+            this.打开记录ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(806, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmOpenFile,
+            this.tsmSaveFile});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // tsmOpenFile
+            // 
+            this.tsmOpenFile.Name = "tsmOpenFile";
+            this.tsmOpenFile.Size = new System.Drawing.Size(122, 26);
+            this.tsmOpenFile.Text = "打开";
+            this.tsmOpenFile.Click += new System.EventHandler(this.tsmOpenFile_Click);
+            // 
+            // tsmSaveFile
+            // 
+            this.tsmSaveFile.Name = "tsmSaveFile";
+            this.tsmSaveFile.Size = new System.Drawing.Size(122, 26);
+            this.tsmSaveFile.Text = "保存";
+            this.tsmSaveFile.Click += new System.EventHandler(this.tsmSaveFile_Click);
+            // 
+            // 格式ToolStripMenuItem
+            // 
+            this.格式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmWordWrap});
+            this.格式ToolStripMenuItem.Name = "格式ToolStripMenuItem";
+            this.格式ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.格式ToolStripMenuItem.Text = "格式";
+            // 
+            // tsmWordWrap
+            // 
+            this.tsmWordWrap.Name = "tsmWordWrap";
+            this.tsmWordWrap.Size = new System.Drawing.Size(152, 26);
+            this.tsmWordWrap.Text = "自动换行";
+            this.tsmWordWrap.Click += new System.EventHandler(this.tsmWordWrap_Click);
+            // 
+            // 样式ToolStripMenuItem
+            // 
+            this.样式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmFont,
+            this.tsmColor});
+            this.样式ToolStripMenuItem.Name = "样式ToolStripMenuItem";
+            this.样式ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.样式ToolStripMenuItem.Text = "样式";
+            // 
+            // tsmFont
+            // 
+            this.tsmFont.Name = "tsmFont";
+            this.tsmFont.Size = new System.Drawing.Size(122, 26);
+            this.tsmFont.Text = "字体";
+            this.tsmFont.Click += new System.EventHandler(this.tsmFont_Click);
+            // 
+            // tsmColor
+            // 
+            this.tsmColor.Name = "tsmColor";
+            this.tsmColor.Size = new System.Drawing.Size(122, 26);
+            this.tsmColor.Text = "颜色";
+            this.tsmColor.Click += new System.EventHandler(this.tsmColor_Click);
+            // 
+            // 打开记录ToolStripMenuItem
+            // 
+            this.打开记录ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmShow,
+            this.tsmViserble});
+            this.打开记录ToolStripMenuItem.Name = "打开记录ToolStripMenuItem";
+            this.打开记录ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.打开记录ToolStripMenuItem.Text = "打开记录";
+            // 
+            // tsmShow
+            // 
+            this.tsmShow.Name = "tsmShow";
+            this.tsmShow.Size = new System.Drawing.Size(122, 26);
+            this.tsmShow.Text = "显示";
+            this.tsmShow.Click += new System.EventHandler(this.tsmShow_Click);
+            // 
+            // tsmViserble
+            // 
+            this.tsmViserble.Name = "tsmViserble";
+            this.tsmViserble.Size = new System.Drawing.Size(122, 26);
+            this.tsmViserble.Text = "隐藏";
+            this.tsmViserble.Click += new System.EventHandler(this.tsmViserble_Click);
+            // 
+            // tbShow
+            // 
+            this.tbShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbShow.Location = new System.Drawing.Point(0, 32);
+            this.tbShow.Multiline = true;
+            this.tbShow.Name = "tbShow";
+            this.tbShow.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbShow.Size = new System.Drawing.Size(806, 608);
+            this.tbShow.TabIndex = 1;
+            // 
+            // panelFather
+            // 
+            this.panelFather.Controls.Add(this.btnviserble);
+            this.panelFather.Controls.Add(this.listbox);
+            this.panelFather.Location = new System.Drawing.Point(0, 32);
+            this.panelFather.Name = "panelFather";
+            this.panelFather.Size = new System.Drawing.Size(211, 611);
+            this.panelFather.TabIndex = 2;
+            // 
+            // btnviserble
+            // 
+            this.btnviserble.Location = new System.Drawing.Point(177, 245);
+            this.btnviserble.Name = "btnviserble";
+            this.btnviserble.Size = new System.Drawing.Size(30, 93);
+            this.btnviserble.TabIndex = 1;
+            this.btnviserble.Text = "<";
+            this.btnviserble.UseVisualStyleBackColor = true;
+            this.btnviserble.Click += new System.EventHandler(this.btnviserble_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.ItemHeight = 15;
+            this.listbox.Location = new System.Drawing.Point(0, 4);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(171, 604);
+            this.listbox.TabIndex = 0;
+            this.listbox.DoubleClick += new System.EventHandler(this.listbox_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 643);
+            this.Controls.Add(this.panelFather);
+            this.Controls.Add(this.tbShow);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelFather.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmOpenFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmSaveFile;
+        private System.Windows.Forms.ToolStripMenuItem 格式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmWordWrap;
+        private System.Windows.Forms.ToolStripMenuItem 样式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmFont;
+        private System.Windows.Forms.ToolStripMenuItem tsmColor;
+        private System.Windows.Forms.ToolStripMenuItem 打开记录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmShow;
+        private System.Windows.Forms.ToolStripMenuItem tsmViserble;
+        private System.Windows.Forms.TextBox tbShow;
+        private System.Windows.Forms.Panel panelFather;
+        private System.Windows.Forms.Button btnviserble;
+        private System.Windows.Forms.ListBox listbox;
+    }
+}
+
---
+++ 记事本应用程序/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 记事本应用程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //创建一个泛型集合用来存储文件的全路径
+        List<string> list = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //加载程序时，隐藏panel
+            panelFather.Visible = false;
+            //窗体加载时取消文本框的自动换行
+            tbShow.WordWrap = false;
+        }
+        #region 隐藏panel
+        private void btnviserble_Click(object sender, EventArgs e)
+        {
+            //点击<按钮时，panel隐藏
+            panelFather.Visible = false;
+        }
+
+        private void tsmShow_Click(object sender, EventArgs e)
+        {
+            //点击显示按钮时，panel显示
+            panelFather.Visible = true;
+        }
+
+        private void tsmViserble_Click(object sender, EventArgs e)
+        {
+            //点击隐藏按钮时，panel隐藏
+            panelFather.Visible = false;
+        }
+        #endregion
+
+        #region 打开文件
+        private void tsmOpenFile_Click(object sender, EventArgs e)
+        {
+            //创建打开对话框的对象
+            OpenFileDialog ofd = new OpenFileDialog();
+            //修改对话框的标题
+            ofd.Title = "请选择您要打开的文件";
+            //设置对话框可以多选
+            ofd.Multiselect = true;
+            //初始化对话框的目录
+            ofd.InitialDirectory = @"C:\Users\TANGJIANYI\Desktop";
+            //设置对话框可以选择的文件类型
+            ofd.Filter = "文本文件|*.txt|所有文件|*.*";
+            //显示对话框
+            ofd.ShowDialog();
+            //获取在对话框选中的文件的全路径
+            string path = ofd.FileName;
+            //获得了用户打开文件的文件名
+            string fileName = Path.GetFileName(path);
+            //将文件名放到ListBox里面去
+            listbox.Items.Add(fileName);
+            //存储文件全路径
+            list.Add(path);
+            //判断如果没有选择文件
+            if (path == "") return;
+            //创建FileStream对象 用来读取文件
+            using (FileStream fsread = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                //设置读取一次读取的字节大小
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                //实际读取的字节大小
+                int r = fsread.Read(buffer, 0, buffer.Length);
+                //将读取到的字节数组转换成Utf-8的格式并赋值给文本框
+                tbShow.Text = Encoding.UTF8.GetString(buffer, 0, r);
+
+            }
+        }
+        #endregion
+
+        #region 保存文件
+        private void tsmSaveFile_Click(object sender, EventArgs e)
+        {
+            //创建一个保存对话框的对象
+            SaveFileDialog sf = new SaveFileDialog();
+            //修改对话框的标题
+            sf.Title = "请选择您需要保存的位置";
+            //初始化对话框的目录
+            sf.InitialDirectory = @"C:\Users\TANGJIANYI\Desktop";
+            //设置对话框可以保存的文件类型
+            sf.Filter = "文本文件|*.txt|所有文件|*.*";
+            //显示对话框
+            sf.ShowDialog();
+            //从对话框获取你所保存的文件全路径
+            string path = sf.FileName;
+            //如果点击取消 没有保存路径
+            if (path == "") return;
+            //创建一个FileStream对象用来写入文件
+            using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                //将文本框里的内容转换成utf-8格式的字节数组
+                byte[] buffer = Encoding.UTF8.GetBytes(tbShow.Text);
+                //将字节数组写入文件流
+                fsWrite.Write(buffer, 0, buffer.Length);
+            }
+            MessageBox.Show("保存成功");
+        }
+
+        #endregion
+
+        #region 字体样式
+        private void tsmFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.ShowDialog();
+            tbShow.Font = fd.Font;
+        }
+
+        private void tsmColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();   
+            cd.ShowDialog();
+            tbShow.ForeColor = cd.Color;
+        }
+        #endregion
+
+        #region 自动换行
+        private void tsmWordWrap_Click(object sender, EventArgs e)
+        {
+            if (tsmWordWrap.Text == "自动换行")
+            {
+                tbShow.WordWrap = true;
+                tsmWordWrap.Text = "取消自动换行";
+            }
+            else
+            {
+                tbShow.WordWrap = false;
+                tsmWordWrap.Text = "自动换行";
+            }
+        }
+        #endregion
+
+        private void listbox_DoubleClick(object sender, EventArgs e)
+        {
+            //获得双击的文件所对应的全路径
+            string path= list[listbox.SelectedIndex];
+            using (FileStream fsRead = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int r = fsRead.Read(buffer, 0, buffer.Length);
+                tbShow.Text=Encoding.UTF8.GetString(buffer, 0, r);
+            }
+        }
+    }
+}
